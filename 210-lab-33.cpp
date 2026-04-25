2284911b279fab0c1c463de9741be47ab61e66a8
msVill <msv.ltmc@gmail.com>
2026-04-24 20:21:31 -0700

    Added: comments for reminders.

--- 210-lab-33.cpp
+++ 210-lab-33.cpp
@@ -4,6 +4,15 @@
 #include <cstdlib>
 #include "Car.h"
 using namespace std;
+// Comsc 210. Section 5068 | Martha S. Villalta | Lab 33
+// Requirements: use std::deque. Use a const and init to 2
+// for init size of deque objects
+// (srd::deque objects of Car type)
+// Use loop run until empty
+// commit at milestones [1]: successfully create a std::deque & pop. 
+//it w/2 Car objects.
+// milestone [2]: code successfully uses a loop that runs until deque isEmpty
+// Inside Loop: branch based on probabilities (rand())
 
 int main() {
 
