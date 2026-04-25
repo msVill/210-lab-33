4652eba5515e5b181ceb21380001832a5fcd1644
msVill <msv.ltmc@gmail.com>
2026-04-24 20:53:57 -0700

    Added: comments for planning and tracking. as well as while-loop shell and initial vals.

--- 210-lab-33.cpp
+++ 210-lab-33.cpp
@@ -24,5 +24,23 @@ int main() {
     for(int i = 0; i < INITIAL_SIZE; i++) {
         lane.push_back(Car()); // of type Car
     }
+
+    // Now I want to display it for Milestone 1 and see initial queue
+    cout << "Initial queue\n";
+    
+    int time = 1;
+    // I need to use a loop and have it run until deque is empty
+    // a for-loop for this will run until the condition is false,
+    // we need to run the loop UNTIL deque is empty
+    while(!lane.empty()) {
+        cout << "Time:" << time; // from the sample output, I need to show what loop operation time period
+        
+        
+        // time needs to increment. from what? forgot to add start val.
+        time++;
+    }
+
+    // to display the queue: use print. Need to until empty - inside while-loop
+
     return 0;
 }
